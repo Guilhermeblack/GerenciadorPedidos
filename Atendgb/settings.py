b0008f916c959a173b5b8f33e8d01bac28846f05
guilhermeBlack <guilherme@black>
2020-10-12 22:24:56 -0300

    'tnt_2'

--- Atendgb/settings.py
+++ Atendgb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '14ns^8=os!q@q(m@wo&1nqo42o_*q*fh3=l$hx!i(#x--@wh4y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -151,7 +151,7 @@ DATABASES = {
 }
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
