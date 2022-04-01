4cbd62b44520cb54c786b5d140dcb15345db0c91
guidev <gui@dev>
2022-03-31 23:05:16 -0300

    ws - error

--- Atendgb/wsgi.py
+++ Atendgb/wsgi.py
@@ -12,6 +12,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",'Atendgb.settings')
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
+
 application = Cling(get_wsgi_application())
 
 
